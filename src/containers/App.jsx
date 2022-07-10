81ab3bea94c103e010459f3a6fda81fb81be20b0
Pabpol <pabpolval@gmail.com>
2022-07-10 01:59:20 -0400

    Fetch data in App.jsx

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/components/App.styl';
 import Header from '../components/Header';
 import About from '../components/About';
@@ -8,12 +8,48 @@ import Academic from '../components/Academic';
 import Skills from '../components/Skills';
 import Interest from '../components/Interest';
 import Languages from '../components/Languages';
+import getData from '../utils/getData';
 
 const App = () => {
+  const [person, setPerson] = useState([]);
+
+
+  useEffect(() => {
+    getPersonData();
+  }, []);
+
+  const getPersonData = async () => {
+    const response = await getData('https://raw.githubusercontent.com/Pabpol/react-eth-challenge/main/data.json');
+    setPerson(response.data);
+  };
+  
+
+  console.log(person.name)
+
+  const {
+    Academic: academics,
+    address,
+    avatar,
+    email,
+    experience: experiences,
+    interest: interests,
+    languages,
+    name,
+    phone,
+    profession,
+    Profile: profile,
+    skills,
+    website,
+  } = person;
   return (
     <>
-      <Header>
-        <About />
+      <Header name={name}>
+        <About
+            address={address}
+            email={email}
+            phone={phone}
+            website={website}
+        />
       </Header>
       <Profile />
       <Experience />
@@ -22,7 +58,7 @@ const App = () => {
       <Interest />
       <Languages />
     </>
-  )
+  );
 };
 
 export default App;
